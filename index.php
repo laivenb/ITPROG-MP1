fe9b7459c0635c9be7b27e600b24f73d218bc20b
laivenb <106794763+laivenb@users.noreply.github.com>
2024-03-10 14:15:16 +0800

    MainFont Addition

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>!!!!!!!!!!!!!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/styles/index.css">
+</head>
+<body>
+<h1>HELLO</h1>
+
+<?php
+echo ("hi");
+?>
+
+<!-- Bootstrap JavaScript Reference-->
+<script src="assets/scripts/bootstrap-min.js"></script>
+
+</body>
+</html>
+
