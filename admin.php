01a6efffb9f14605dabec31cd90ac7d83cef1ae4
laivenb <106794763+laivenb@users.noreply.github.com>
2024-03-12 18:26:13 +0800

    created a separate file for side bar

--- admin.php
+++ admin.php
@@ -5,41 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SkyCuisine</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Required Elements per Page Start -->
+    <?php include 'sideBar.php'; ?>                              <!-- SideBar .php -->
+    <?php include './assets/scripts/frameworkLib.php'; ?>        <!-- Framework PHP Script Reference-->
+    <!-- Required Elements per Page End -->
     <link rel="stylesheet" href="assets/styles/admin.css">
 </head>
 
 <body>
-        <!-- Required Elements per Page Start -->
-        <?php include './assets/scripts/frameworkLib.php'; ?>        <!-- Framework PHP Script Reference-->
-        <!-- Required Elements per Page End -->
-
-        <!-- SideBar Start -->
-        <div class="sideBar">
-            <!-- User Profile Start -->
-            <div class="user">
-                <img src="./assets/images/admin/anonUser.png" alt="" class="userProImg">
-                <div class="nameDisplay">
-                    <h2 class="HnameDisplay">Admin Admin</h2>
-                    <p>Project Manager</p>
-                </div>
-            </div>
-            <!-- User Profile End -->
-            <!-- Side Bar List Start -->
-            <ul class="sideBarList">
-                <li><img src="./assets/images/admin/dashboard.png" alt=""><p>Dashboard</p></li>
-                <li><img src="./assets/images/admin/item.png" alt=""><p>Manage Item</p></li>
-                <li><img src="./assets/images/admin/record.png" alt=""><p>Order Management</p></li>
-                <li><img src="./assets/images/admin/core.png" alt=""><p>Core Members</p></li>
-            </ul>
-            <!-- Side Bar List End -->
-            <!-- Side Bar Log Out Start -->
-            <ul class="logOutList">
-                <li><img src="./assets/images/admin/logOut.png" alt=""><p>Log Out</p></li>
-            </ul>
-            <!-- Side Bar Log Out End -->
-        </div>
-        <!-- SideBar End -->
-
         <!-- Welcome Logo Start -->
         <div class="adminBanner">
             <img src="./assets/images/admin/logoBlack.png" alt="" class="logoBlackImg">
---
+++ sideBar.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" href="assets/styles/admin.css">
+
+<!-- SideBar Start -->
+<div class="sideBar">
+    <!-- User Profile Start -->
+    <div class="user">
+        <img src="./assets/images/admin/anonUser.png" alt="" class="userProImg">
+        <div class="nameDisplay">
+            <h2 class="HnameDisplay">Admin Admin</h2>
+            <p>Project Manager</p>
+        </div>
+    </div>
+    <!-- User Profile End -->
+    <!-- Side Bar List Start -->
+    <ul class="sideBarList">
+        <li><img src="./assets/images/admin/dashboard.png" alt=""><a class="sideBarRef" href="adminDashboard.php"><p>Dashboard</p></a></li>
+        <li><img src="./assets/images/admin/item.png" alt=""><a class="sideBarRef" href="adminItem_Manager.php"><p>Manage Item</p></a></li>
+        <li><img src="./assets/images/admin/record.png" alt=""><a class="sideBarRef" href="adminOrder.php"><p>Order Management</p></a></li>
+        <li><img src="./assets/images/admin/core.png" alt=""><a class="sideBarRef" href="adminMembers.php"><p>Core Members</p></a></li>
+    </ul>
+    <!-- Side Bar List End -->
+    <!-- Side Bar Log Out Start -->
+    <ul class="logOutList">
+        <li><img src="./assets/images/admin/logOut.png" alt=""><p>Log Out</p></li>
+    </ul>
+    <!-- Side Bar Log Out End -->
+</div>
+<!-- SideBar End -->
