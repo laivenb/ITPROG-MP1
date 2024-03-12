8657bb4f73ee394ef94e5efbd3992cbc9aa8797e
laivenb <106794763+laivenb@users.noreply.github.com>
2024-03-12 17:40:50 +0800

    added admin welcome page

--- index.php
+++ index.php
@@ -5,16 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SkyCuisine</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Required Elements per Page Start -->
+    <?php include 'header.php'; ?>                               <!-- Header .php -->
+    <?php include './assets/scripts/frameworkLib.php'; ?>        <!-- Framework PHP Script Reference-->
+    <!-- Required Elements per Page End -->
     <link rel="stylesheet" href="assets/styles/index.css">
 </head>
 
 
 <body>
-        <!-- Required Elements per Page Start -->
-        <?php include 'header.php'; ?>                               <!-- Header .php -->
-        <?php include './assets/scripts/frameworkLib.php'; ?>        <!-- Framework PHP Script Reference-->
-        <!-- Required Elements per Page End -->
-
         <!-- Banner Start -->
         <div class="parentBanner">
             <div class="banner">
